fc666e29227634def51aed1896d4c94e75e6b9f5
P Manohar reddy <zapmanuu7@gmail.com>
2026-03-29 20:30:59 +0530

    Add project assets: screenshots, state pages, and data files

---
+++ states/karnataka.js
@@ -0,0 +1,14 @@
+loadStateData({
+    tradition:{
+        text:"Yakshagana, temple festivals.",
+        image:"images/karnataka_tradition.jpg"
+    },
+    culture:{
+        text:"Hampi, Vijayanagara empire.",
+        image:"kacult.jpg"  
+    },
+    food:{
+        text:"Bisi Bele Bath, Mysore Pak.",
+        image:"kafood.jpg" 
+    }
+});
